2265bbe5037923925ce5cd1c041dfee9f618846c
Davnoronha <davidnd97@gmail.com>
2025-09-24 17:09:23 -0300

    fix:disabling ssr on page gift

--- app/your-gift/page.tsx
+++ app/your-gift/page.tsx
@@ -1,7 +1,11 @@
-import RedeemPage from "@/app/components/RedeemPage"
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import RedeemPage from "@/app/components/RedeemPage";
 
 export default function YourGift() {
   return (
     <RedeemPage />
-  )
+  );
 }

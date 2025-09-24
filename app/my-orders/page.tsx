c5133b33b78fd539a524d15b5a5bb931c14fb626
Davnoronha <davidnd97@gmail.com>
2025-09-24 17:05:43 -0300

    fix:disabling ssr on page

--- app/my-orders/page.tsx
+++ app/my-orders/page.tsx
@@ -1,5 +1,8 @@
-"use client"
-import RedeemPage from "@/app/components/RedeemPage"
+"use client";
+
+export const dynamic = "force-dynamic";
+
+import RedeemPage from "@/app/components/RedeemPage";
 
 export default function MyOrders() {
   return (
